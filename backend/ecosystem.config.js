6c801140751015511d53e7b5c82ebb1b2b9f647c
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-09-04 11:21:44 +0300

    Обновлена ссылка на репозиторий в `ecosystem.config.js`: заменено на `cms.nekrasovka.v2.git`.

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       user: "superuser",
       host: "10.10.0.37",
       ref: "origin/master",
-      repo: "git@github.com:nekrasovka-library/cms.nekrasovka.git",
+      repo: "git@github.com:nekrasovka-library/cms.nekrasovka.v2.git",
       path: "/var/www/cms.test.nekrasovka",
       "post-deploy":
         "cd /var/www/cms.test.nekrasovka/current/frontend && npm install && npm run build && cd ../backend && npm install && pm2 reload /var/www/cms.test.nekrasovka/current/backend/ecosystem.config.js --env production",

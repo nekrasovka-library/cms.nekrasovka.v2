75c33f0d4a316deaa18d0ce7887ed1d2799282d0
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-10-16 13:24:29 +0300

    Обновлена система передачи настроек страниц: добавлена фильтрация по `url`, переработан вызов функции `handlePageSettings` для передачи полного объекта страницы. Обновлен файл базы данных `database.sqlite`.

--- frontend/src/pages/editor/components/BlockContent/block.content.constants.js
+++ frontend/src/pages/editor/components/BlockContent/block.content.constants.js
@@ -25,12 +25,6 @@ export const getBlockSettingsTypes = (props, type) => {
         label: "Отменено",
       },
     },
-    is_public: {
-      element: Switch,
-      params: {
-        label: "Опубликовано",
-      },
-    },
   };
 
   return BLOCK_SETTINGS_TYPES[type];
